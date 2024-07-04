578851e15ee20771ff389dde3b1eebc8a1975a09
gitea <admin@admin.com>
2024-07-04 10:02:17 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Login_Exception : Exception
+{
+    public Login_Exception() : base()
+    {}
+    public Login_Exception(string message) : base(message)
+    {}
+}
+
+public class Ali
+{
+    public static void Main()
+    {
+        string Password = "";
+        try
+        {
+            Console.WriteLine("Please! Enter your Password: ");
+            Password = Console.ReadLine();
+
+            if (Password == "ALI")
+            {
+                Console.WriteLine("Login Successful!");
+            }
+            else
+            {
+                throw new Login_Exception("Wrong Password! Try Again!");
+            }
+        }
+        catch(Login_Exception LE)
+        {
+            Console.WriteLine(LE.Message);
+        }
+    }
+}
